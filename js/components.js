588112bf4cf4ec506fae45a19b867994cfc71022
unknown <nehemimosqueda8@gmail.com>
2023-12-09 21:28:54 -0300

    agregue clima x horas y licencia

--- js/components.js
+++ js/components.js
@@ -7,24 +7,22 @@ async function searchWeather() {
     const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${cityInput}&days=3&lang=${lang}`);
     const data = await response.json();
 
-      // Verificar si se obtuvieron datos correctamente
       if (data.error) {
           alert('No se pudo obtener la información del clima. Inténtalo de nuevo.');
           return;
       }
 
-      // Mostrar información del clima en los elementos HTML correspondientes
       document.getElementById('today-description').innerText = data.forecast.forecastday[0].day.condition.text;
       document.getElementById('tomorrow-description').innerText = data.forecast.forecastday[1].day.condition.text;
       document.getElementById('day-after-tomorrow-description').innerText = data.forecast.forecastday[2].day.condition.text;
 
-      // Actualizar las imágenes según el clima (necesitarás manejar esto dependiendo de la API que utilices)
-      // data.forecast.forecastday[0].day.condition.icon puede ser la URL de la imagen para el clima actual, por ejemplo
-
-      // Ejemplo de cómo podrías usar la URL de la imagen proporcionada por la API
       document.getElementById('today').getElementsByTagName('img')[0].src = data.forecast.forecastday[0].day.condition.icon;
       document.getElementById('tomorrow').getElementsByTagName('img')[0].src = data.forecast.forecastday[1].day.condition.icon;
       document.getElementById('day-after-tomorrow').getElementsByTagName('img')[0].src = data.forecast.forecastday[2].day.condition.icon;
+
+      document.getElementById('today-temperature').innerText = data.forecast.forecastday[0].day.avgtemp_c + '°C';
+document.getElementById('tomorrow-temperature').innerText = data.forecast.forecastday[1].day.avgtemp_c + '°C';
+document.getElementById('day-after-tomorrow-temperature').innerText = data.forecast.forecastday[2].day.avgtemp_c + '°C';
 
   } catch (error) {
       console.error('Hubo un error al obtener datos del clima:', error);
@@ -39,17 +37,12 @@ async function getWeatherByUserLocation() {
               const lat = position.coords.latitude;
               const lon = position.coords.longitude;
 
-              const apiKey = 'c4b6d68355b64c67840235826230212'; // Reemplaza 'TU_API_KEY' con tu clave de API de Weather API
+              const apiKey = 'c4b6d68355b64c67840235826230212'; 
 
               const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${lon}`);
               const data = await response.json();
 
               document.getElementById('city-input').value = data.location.name;
-
-              // Mostrar información del clima
-              // ...
-
-              // Llamar a la función searchWeather para cargar los datos del clima por defecto
               searchWeather();
           });
       } else {
@@ -60,18 +53,56 @@ async function getWeatherByUserLocation() {
   }
 }
 
-// Llamar a la función para obtener el clima por la ubicación del usuario al cargar la página
 window.onload = getWeatherByUserLocation;
 
-function toggleTheme() {
-  const body = document.body;
-  body.classList.toggle('dark-mode');
+async function getHourlyForecast(city, index) {
+  const apiKey = 'c4b6d68355b64c67840235826230212'; 
+  const lang = 'es';
 
-  // Obtenemos los elementos de los iconos
-  const darkIcon = document.getElementById('dark-icon');
-  const lightIcon = document.getElementById('light-icon');
+  try {
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=1&lang=${lang}`);
+      const data = await response.json();
 
-  // Alternamos la visibilidad de los iconos
-  darkIcon.style.display = darkIcon.style.display === 'none' ? 'block' : 'none';
-  lightIcon.style.display = lightIcon.style.display === 'none' ? 'block' : 'none';
+      if (data.error) {
+          alert('No se pudo obtener la información del clima por horas. Inténtalo de nuevo.');
+          return null;
+      }
+
+      return data.forecast.forecastday[0].hour;
+  } catch (error) {
+      console.error('Hubo un error al obtener datos del clima por horas:', error);
+      alert('Hubo un error al obtener datos del clima por horas. Por favor, inténtalo de nuevo más tarde.');
+      return null;
+  }
 }
+
+// Función para mostrar u ocultar el pronóstico por horas al hacer clic en la flecha
+async function toggleHourlyForecast(cardId) {
+    const card = document.getElementById(cardId);
+    const hourlyForecastDiv = card.querySelector('.hourly-forecast');
+  
+    if (hourlyForecastDiv.style.display === 'block') {
+        hourlyForecastDiv.style.display = 'none';
+    } else {
+        const city = document.getElementById('city-input').value;
+        const index = cardId === 'today' ? 0 : cardId === 'tomorrow' ? 1 : 2;
+        const hourlyForecast = await getHourlyForecast(city, index);
+  
+        if (hourlyForecast) {
+            const hourlyForecastContent = hourlyForecast.map(hour => {
+                return `
+                    <div class="hourly-weather">
+                        <h3>============================</h3>
+                        <h3>${hour.time.slice(11, 16)}</h3>
+                        <img src="${hour.condition.icon}" alt="${hour.condition.text}">
+                        <p>${hour.condition.text}</p>
+                        <p>${hour.temp_c}°C</p>
+                    </div>
+                `;
+            }).join('');
+  
+            hourlyForecastDiv.innerHTML = hourlyForecastContent;
+            hourlyForecastDiv.style.display = 'block';
+        }
+    }
+  }
